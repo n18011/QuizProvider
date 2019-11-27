d07ba3e95d71fb6cde51c863fbe10833a4597275
n18011 <n18011@std.it-college.ac.jp>
2019-11-27 15:38:35 +0900

    append TODOs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+export default () => {
+  // TODO:reducerの設定
+  // TODO:context of reducerの設定
+  // TODO:themeの設定
+
+  // TODO:localforageの接続設定
+  // TODO:context of localforageの設定
+
+  // TODO:routingの設定
+  // TODO:HeaderTemplate componentの作成
+
+  //TODO:contentの作成
+  //TODO:main contentの作成
+  //TODO:component(main)の作成
+  //TODO:sub content(ranking)の作成
+  //TODO:ranking用のfirestoreの作成
+  //TODO:component(sub)の作成
+
+  //TODO:seajのdata整形
+  //TODO:seajのdataをfirestoreへ
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
 
-export default App;
