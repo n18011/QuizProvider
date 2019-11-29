bba2166df9649d646b0216c8833c046a60db66cc
n18011 <n18011@std.it-college.ac.jp>
2019-11-29 09:42:12 +0900

    cached get

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -11,9 +11,19 @@ const reducer = (state, action) => {
       return { ...state, isDark: !state.isDark }
     case 'sel_quiz':
       //TODO:read only cache if !cache network get data
+      const docRef = firestore.collection('seaj').doc(action.payload)
       const data = []
+      docRef.get({source: 'cache'})
+      .then(doc => {
+        console.log('Cached document data: ', doc.data())
+        data.push(doc.data())
+      })
+      .catch(error => {
+        console.log('Error getting cached document => ', error)
+      })
       // network get data
-      firestore.collection('seaj').doc(action.payload).get().then(doc => {
+      /*
+      docRef.get().then(doc => {
         if (doc.exists) {
         console.log('doc.data => ', doc.data())
         data.push(doc.data())
@@ -24,6 +34,7 @@ const reducer = (state, action) => {
       .catch(error => {
         console.log('Error getting document', error)
       })
+      */
       return { ...state, quizData: data}
     default:
       throw new Error()
